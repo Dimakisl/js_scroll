6c7661a4679416280c4b7e1cf2d6bc724eb221fb
Dimakisl <39777576+Dimakisl@users.noreply.github.com>
2020-05-16 20:34:49 +0300

    add files
    add files

---
+++ main.js
@@ -0,0 +1,12 @@
+function scrollTo(element){
+	window.scroll({
+		left: 0,
+		top: element.offsetTop,
+		behavior: 'smooth'
+	});
+}
+
+let button = document.querySelector('.button');
+let footer = document.querySelector('.footer');
+
+button.addEventListener('click', () => {scrollTo(footer)});
